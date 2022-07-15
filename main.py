e78cea27ba9b9b6536f53ff5a599bc8296d5e57d
Anthart <51451673+Anthart@users.noreply.github.com>
2022-07-14 23:14:39 -0500

    actualizacion prompt

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ if __name__ == "__main__":
     I'm classifying these words into \"very easy\", \"easy\", \"neutral\", \"difficult\" and \"very difficult\".\n
     After reading the fragment \" However, no defects in axon pathfinding along the monosynaptic reflex arc or in muscle
      spindle differentiation have been noted in PV KO mice, which develop normally and show no apparent changes in their
-     behavior or physical activity (Schwaller et al. 1999). \". I find that expression \"spindle\" is neutral
+     behavior or physical activity (Schwaller et al. 1999). \". I find that word \"spindle\" is neutral
 
     \n ### \n
 
     I'm reading fragments from the source, and some words are not easy to understand.
     I'm classifying these words into \"very easy\", \"easy\", \"neutral\", \"difficult\" and \"very difficult\".\n
     After reading the fragment \" I will sprinkle clean water on you, and you shall be clean: from all your filthiness,
-    and from all your idols, will I cleanse you. \". I find that expression \"filthiness\" is easy
+    and from all your idols, will I cleanse you. \". I find that word \"filthiness\" is easy
 
     \n ### \n
 
     I'm reading fragments from the @recurso, and some words are not easy to understand.
     I'm classifying these words into \"very easy\", \"easy\", \"neutral\", \"difficult\" and \"very difficult\".\n
-    After reading the fragment @oracion I find that expression @aEvaluar is"""
+    After reading the fragment @oracion I find that word @aEvaluar is"""
 
     df = pd.read_excel("corpus/labeled-lcp_single_test.xlsx")
 
-    datos = df.loc[0:10, ["source", "sentence", "token", "complexity"]]
+    datos = df.loc[210:240, ["source", "sentence", "token", "complexity"]]
 
     resultado = palabras_complejas(datos, prompt)
 
