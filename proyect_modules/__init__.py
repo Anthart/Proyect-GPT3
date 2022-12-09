e847666b5b8a88974b1991e5feb592dcbdc43a46
anthart <ffarteaga98@gmail.com>
2022-12-08 22:02:22 -0500

    añadido setup.py

--- proyect_modules/__init__.py
+++ proyect_modules/__init__.py
@@ -3,4 +3,5 @@ __version__ = "0.0.1"
 __author__ = "Anthart"
 __email__ = "ffarteaga98@gmail.com"
 __description__ = "Proyecto GPT3"
+__url__ = "https://github.com/Anthart/Proyect-GPT3.git"
 
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="proyectT",
+    version="0.0.1",
+    description="Proyecto GPT3",
+    author="Anthart",
+    author_email="ffarteaga98@gmail.com",
+    url="https://github.com/Anthart/Proyect-GPT3.git",
+    packages=['proyect_modules'],
+    zip_safe=False,
+    install_requires=["transformers", "openai", "pandas"]
+)
